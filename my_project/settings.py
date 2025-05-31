13e40fbc06f92fee01db5e77bb40a232aa2c268b
achristoforou1 <a_christoforou1@yahoo.com>
2025-05-31 12:10:36 +0300

    heroku added and debug to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-urxga7)s=!8h18p779cf@hex_-5w252_6(45hj0xf5$1kl)0=#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
